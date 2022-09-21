ca95d4ef32e807707296fb37a1ffe6dad086fd92
William Kennedy <bill@ardanlabs.com>
2022-09-21 14:13:39 -0400

    schema changes

--- business/data/dbschema/sql/schema.sql
+++ business/data/dbschema/sql/schema.sql
@@ -1,46 +1,50 @@
+-- Version: 1.1
+-- Description: Create table accounts
+CREATE TABLE accounts (
+  address VARCHAR(42),
+  nonce   INT
+
+  PRIMARY KEY (address)
+);
+
+-- Version: 1.2
 -- Description: Create table bets
 CREATE TABLE bets (
-    bet_id        UUID,
-    status        TEXT,
-    description   TEXT,
-    terms         TEXT,
-    amount        INT,
-    expiration    TIMESTAMP,
-    moderator_id  UUID,
-    created       TIMESTAMP,
-    updated       TIMESTAMP,
+    bet_id            UUID,
+    status            INT,
+    description       VARCHAR(240),
+    terms             VARCHAR(240),
+    amount            INT,
+    moderator_address UUID,
+    expiration        TIMESTAMP,
+    created           TIMESTAMP,
+    updated           TIMESTAMP,
 
     PRIMARY KEY (bet_id),
-    FOREIGN KEY (moderator_id) REFERENCES accounts(account_id) ON DELETE CASCADE
+    FOREIGN KEY (moderator_address) REFERENCES accounts(address) ON DELETE CASCADE
 );
 
+-- Version: 1.3
 -- Description: Create table bets_players
 CREATE TABLE bets_players (
-    bet_id    UUID,
-    account_id UUID,
+    bet_id  UUID,
+    address VARCHAR(42),
 
-    PRIMARY KEY (bet_id, account_id),
+    PRIMARY KEY (bet_id, address),
     FOREIGN KEY (bet_id) REFERENCES bets(bet_id) ON DELETE CASCADE,
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
+    FOREIGN KEY (address) REFERENCES accounts(address) ON DELETE CASCADE
 );
 
+-- Version: 1.4
 -- Description: Create table bets_signatures
 CREATE TABLE bets_signatures (
-    bet_id      UUID,
-    account_id  UUID,
-    signature   TEXT,
-    nonce       INT,
-    date        TIMESTAMP,
+    bet_id     UUID,
+    address    VARCHAR(42),
+    nonce      INT,
+    signature  VARCHAR(66),
+    date       TIMESTAMP,
 
     PRIMARY KEY (bet_id, player_id),
     FOREIGN KEY (bet_id) REFERENCES bets(bet_id) ON DELETE CASCADE,
-    FOREIGN KEY (account_id) REFERENCES accounts(account_id) ON DELETE CASCADE
-);
-
--- Description: Create table accounts
-CREATE TABLE accounts (
-  account_id UUID,
-  address    TEXT,
-
-  PRIMARY KEY (account_id)
+    FOREIGN KEY (address) REFERENCES accounts(address) ON DELETE CASCADE
 );
