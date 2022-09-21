e5b24633fa8d02578809319060f77a5a3ddbfa78
Leandro Toledo <leandro.toledo@ardanlabs.com>
2022-09-21 19:38:20 +0100

    fixed double quotes

--- business/data/dbschema/sql/schema.sql
+++ business/data/dbschema/sql/schema.sql
@@ -1,6 +1,6 @@
 -- Version: 1.1
 -- Description: Create status enums.
-CREATE TYPE STATUSES AS ENUM ('negotiation', 'signatures', "moderation", "live", "cancel");
+CREATE TYPE STATUSES AS ENUM ('negotiation', 'signatures', 'moderation', 'live', 'cancel');
 
 -- Version: 1.2
 -- Description: Create table accounts
