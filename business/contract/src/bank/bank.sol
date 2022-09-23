3eff2076d77af4171678a59f24ce143025d836b0
Jason Hutchinson <jason@ardanlabs.com>
2022-09-23 11:03:37 -0500

    minimize SC storage

--- business/contract/src/bank/bank.sol
+++ business/contract/src/bank/bank.sol
@@ -11,20 +11,8 @@ contract Bank {
     // accountBalances represents the amount of money an account has available.
     mapping (address => uint256) private accountBalances;
 
-    // betPool represents the amount of money stored in the pool for a given bet.
-    mapping (string => uint256) private betPool;
-
-    // betExpires represents the expiration date of a given bet.
-    mapping (string => uint256) private betExpires;
-
-    // betModerator represents the moderator for a given bet.
-    mapping (string => address) private betModerator;
-
-    // accountNonce represents the current nonce for a bettor or moderator.
-    mapping (address => uint) private accountNonce;
-
-    // amountBet represents the amount the account has bet thus far (deprecated)
-    mapping (address => uint256) private amountBet;
+    // amountLocked represents the amount of money locked in for live bets.
+    mapping (string => uint256) private amountLocked;
 
     // EventLog provides support for external logging.
     event EventLog(string value);
@@ -65,9 +53,6 @@ contract Bank {
         bytes32[] memory r,
         bytes32[] memory s
     ) onlyOwner public {
-        // Initialize bet information.
-        betExpires[betId] = expiration;
-        betModerator[betId] = moderator;
 
         // Loop through bettor information and signatures.
         for (uint bettor = 0; bettor < nonce.length; bettor++) {
@@ -83,17 +68,9 @@ contract Bank {
                 revert("insufficient funds");
             }
 
-            // Ensure the bettor's nonce is valid.
-            if (nonce[bettor] != accountNonce[bettorAddress] + 1) {
-                revert("invalid bettor nonce");
-            }
-
             // Move the funds from the bettor's balance into the betting pool.
-            betPool[betId] += amount;
+            amountLocked[betId] += amount;
             accountBalances[bettorAddress] -= amount;
-
-            // Increment account nonce for later bets.
-            accountNonce[bettorAddress]++;
 
             emit EventLog(string.concat("betId[", betId, "] bettor[", Error.Addrtoa(bettorAddress), "] bet[", Error.Itoa(amount), "]"));
         }
@@ -103,34 +80,25 @@ contract Bank {
     function ReconcileSigned(
         string memory betId,
         address[] memory winners,
+        address moderator,
         uint nonce,
         uint8 v,
         bytes32 r,
         bytes32 s
     ) onlyOwner public {
-        // Bet can only be reconciled within 24 hours after expiration.
-        if (betExpires[betId] < block.timestamp || block.timestamp > betExpires[betId] + 24 hours) {
-            revert("bet cannot be reconciled at this time");
-        }
 
         // Hash the reconciliation information.
         bytes32 hash = hashReconcile(betId, winners, nonce);
 
         // Retrieve the moderator from the signed hash and signature.
-        address moderator = ecrecover(hash, v, r, s);
+        address validateModerator = ecrecover(hash, v, r, s);
 
-        // Ensure the address matches the bet's moderator on file.
-        if (moderator != betModerator[betId]) {
-            revert("not authorized to reconcile this bet");
-        }
-
-        // Ensure the moderator's nonce is valid.
-        if (nonce != accountNonce[moderator] + 1) {
-            revert("invalid moderator nonce");
+        if (moderator != validateModerator) {
+            revert("invalid moderator signature");
         }
 
         // Set winnings amount per winner.
-        uint256 winnings = betPool[betId] / winners.length;
+        uint256 winnings = amountLocked[betId] / winners.length;
 
         // Reconcile winnings.
         for (uint winner = 0; winner < winners.length; winner++) {
@@ -139,79 +107,12 @@ contract Bank {
         }
 
         // Empty bet pool.
-        betPool[betId] = 0;
-
-        // Increment moderator nonce.
-        accountNonce[moderator]++;
-    }
-
-    // PlaceBet moves money from accountBalances to amountBet.
-    function PlaceBet(address person, uint256 amount, uint256 fee) onlyOwner public {
-
-        // Subtract the fee from the account balance immediately.
-        if (accountBalances[person] < fee) {
-            accountBalances[Owner] += accountBalances[person];
-            accountBalances[person] = 0;
-        } else {
-            accountBalances[Owner] += fee;
-            accountBalances[person] -= fee;
-        }
-
-        // Check if the balance is enough to accommodate the bet.
-        if (accountBalances[person] < amount) {
-            revert("account balance too low");
-        }
-
-        amountBet[person] = amount;
-        emit EventLog(string.concat("bet[", Error.Itoa(amount), "] total[", Error.Itoa(amountBet[person]), "]"));
-    }
-
-    // Reconcile settles the accounting for a game that was played.
-    function Reconcile(address winner, address loser, uint256 amount, uint256 fee) onlyOwner public {
-
-        // Subtract the fee from the loser's account balance immediately.
-        if (accountBalances[loser] < fee) {
-            // TODO: Should this revert, return, or should reconcile be allowed
-            // to continue, zeroing out the bet for both parties?
-            emit EventLog("loser balance too low, taking remainder as fee");
-            accountBalances[Owner] += accountBalances[loser];
-            accountBalances[loser] = 0;
-        } else {
-            accountBalances[Owner] += fee;
-            accountBalances[loser] -= fee;
-        }
-
-        // Reconcile the winnings.
-        if (accountBalances[loser] < amount) {
-            emit EventLog("loser balance too low, moving remainder to winner account");
-            accountBalances[winner] += accountBalances[loser];
-            accountBalances[loser] = 0;
-        } else {
-            accountBalances[winner] += amount;
-            accountBalances[loser] -= amount;
-        }
-
-        // Remove amount from account bets, be cautious of uint overflow.
-        if (amountBet[winner] < amount) {
-            amountBet[winner] = 0;
-        } else {
-            amountBet[winner] -= amount;
-        }
-
-        // TODO: If the amount transferred above was less than the bet amount,
-        // does the loser still retain some "owed" amount?
-        if (amountBet[loser] < amount) {
-            amountBet[loser] = 0;
-        } else {
-            amountBet[loser] -= amount;
-        }
-
-        emit EventLog(string.concat("winner balance[", Error.Itoa(accountBalances[winner]), "] loser balance[", Error.Itoa(accountBalances[loser]), "]"));
+        amountLocked[betId] = 0;
     }
 
     // AccountBalance returns the specified account's balance and amount bet.
-    function AccountBalance(address account) onlyOwner view public returns (uint[2] memory) {
-        return [accountBalances[account], amountBet[account]];
+    function AccountBalance(address account) onlyOwner view public returns (uint) {
+        return accountBalances[account];
     }
 
     // =========================================================================
@@ -219,7 +120,7 @@ contract Bank {
 
     // Balance returns the balance of the caller.
     function Balance() view public returns (uint) {
-        return accountBalances[msg.sender] - amountBet[msg.sender];
+        return accountBalances[msg.sender];
     }
 
     // Deposit the given amount to the account balance.
@@ -232,13 +133,7 @@ contract Bank {
     function Withdraw() payable public {
         address payable account = payable(msg.sender);
 
-        // IF AMOUNT BET IS > BALANCE REVERT
-        // WE DON'T WANT THE UNSIGNED INT TO ROLLOVER
-        if (amountBet[msg.sender] > accountBalances[msg.sender]) {
-            revert("not enough balance");
-        }
-
-        uint bal = accountBalances[msg.sender] - amountBet[msg.sender];
+        uint bal = accountBalances[msg.sender];
         if (bal == 0) {
             revert("not enough balance");
         }
