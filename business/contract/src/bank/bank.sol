59267ba06c24a61dfc0ec497cb68b58550c5f229
Jason Hutchinson <jason@ardanlabs.com>
2022-09-26 10:00:45 -0500

    store additional bet information in smart contract

--- business/contract/src/bank/bank.sol
+++ business/contract/src/bank/bank.sol
@@ -5,14 +5,23 @@ import "./error.sol";
 
 contract Bank {
 
+    // BetChannel represents an individual bet's structure.
+    struct BetChannel {
+        mapping (address => bool) Participants;
+        uint256 NumParticipants;
+        address Moderator;
+        uint256 Pool;
+        uint256 Expiration;
+    }
+
     // Owner represents the address who deployed the contract.
     address public Owner;
 
     // accountBalances represents the amount of money an account has available.
     mapping (address => uint256) private accountBalances;
 
-    // amountLocked represents the amount of money locked in for live bets.
-    mapping (string => uint256) private amountLocked;
+    // betsMap represents current bets, organized by Bet ID.
+    mapping (string => BetChannel) private betsMap;
 
     // EventLog provides support for external logging.
     event EventLog(string value);
@@ -55,6 +64,11 @@ contract Bank {
         bytes32[] memory s
     ) onlyOwner public {
 
+        // Initialize the new bet's information.
+        betsMap[betId].NumParticipants = bettors.length;
+        betsMap[betId].Moderator = moderator;
+        betsMap[betId].Expiration = expiration;
+
         // Loop through bettor information and signatures.
         for (uint bettor = 0; bettor < bettors.length; bettor++) {
 
@@ -75,8 +89,11 @@ contract Bank {
                 revert("insufficient funds");
             }
 
+            // Store the bettor's address in the bet's Participants map.
+            betsMap[betId].Participants[bettorAddress] = true;
+
             // Move the funds from the bettor's balance into the betting pool.
-            amountLocked[betId] += amount;
+            betsMap[betId].Pool += amount;
             accountBalances[bettorAddress] -= amount;
 
             emit EventLog(string.concat("betId[", betId, "] bettor[", Error.Addrtoa(bettorAddress), "] bet[", Error.Itoa(amount), "]"));
@@ -105,7 +122,7 @@ contract Bank {
         }
 
         // Set winnings amount per winner.
-        uint256 winnings = amountLocked[betId] / winners.length;
+        uint256 winnings = betsMap[betId].Pool / winners.length;
 
         // Reconcile winnings.
         for (uint winner = 0; winner < winners.length; winner++) {
@@ -114,7 +131,62 @@ contract Bank {
         }
 
         // Empty bet pool.
-        amountLocked[betId] = 0;
+        betsMap[betId].Pool = 0;
+    }
+
+    // CancelBet allows participants to cancel a bet 30 days after cancelation.
+    function CancelBet(
+        string memory betId,
+        address[] memory bettors,
+        uint nonce,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) public {
+        // Ensure the bet has expired.
+        if (block.timestamp < betsMap[betId].Expiration + 30 days) {
+            revert("bets may only be canceled 30+ days after expiration");
+        }
+
+        // Ensure the bet has not been reconciled.
+        if (betsMap[betId].Pool > 0) {
+            revert("bets may only be canceled if unreconciled");
+        }
+
+        // Ensure the bettors provided match the bet's participants.
+        if (betsMap[betId].NumParticipants != bettors.length) {
+            revert("invalid participants");
+        }
+        for (uint i = 0; i < bettors.length; i++) {
+            if (!betsMap[betId].Participants[bettors[i]]) {
+                revert ("invalid participant");
+            }
+        }
+
+        // Hash the cancelation information.
+        bytes32 hash = hashCancel(betId, bettors, nonce);
+
+        // Get the address of the canceler.
+        address canceler = ecrecover(hash, v, r, s);
+
+        // Ensure the canceler is one of the participants.
+        if (!betsMap[betId].Participants[canceler]) {
+            revert("canceler is not authorized to cancel this bet");
+        }
+
+        // Ensure the canceler is the one making the request.
+        if (canceler != msg.sender) {
+            revert("canceler did not request cancelation");
+        }
+
+        // Calculate the bet amount.
+        uint256 amount = betsMap[betId].Pool / betsMap[betId].NumParticipants;
+
+        // Refund the bet amount to all participants.
+        for (uint i = 0; i < bettors.length; i++) {
+            accountBalances[bettors[i]] += amount;
+            betsMap[betId].Pool -= amount;
+        }
     }
 
     // AccountBalance returns the specified account's balance and amount bet.
@@ -163,6 +235,12 @@ contract Bank {
     // of a given bet.
     function hashReconcile(string memory betId, address[] memory winners, address moderator, uint nonce) internal pure returns (bytes32) {
         return ethSignedHash(keccak256(abi.encodePacked(betId, winners, moderator, nonce)));
+    }
+
+    // hashCancel is an internal function to create a hash for the cancelation
+    // of a given bet.
+    function hashCancel(string memory betId, address[] memory bettors, uint nonce) internal pure returns (bytes32) {
+        return ethSignedHash(keccak256(abi.encodePacked(betId, bettors, nonce)));
     }
 
     // ethSignedHash is an internal function which signs a hash with the
